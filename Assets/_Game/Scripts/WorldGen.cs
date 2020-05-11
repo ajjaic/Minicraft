9e93da80ab0db205a9c81d1112c6ef5a73c04fe6
AJ <ajjaic@cseries.us>
2020-05-11 14:11:51 -0400

    initial commit

---
+++ Assets/_Game/Scripts/WorldGen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGen : MonoBehaviour
+{
+    [SerializeField] private GameObject cube;
+    [SerializeField] private int worldSize = 2;
+    
+    void Start()
+    {
+        for (int width = 0; width < worldSize; width++)
+        {
+            for (int height = 0; height < worldSize; height++)
+            {
+                for (int depth = 0; depth < worldSize; depth++)
+                {
+                    Vector3 newPos = new Vector3(width, height, depth);
+                    Instantiate(cube, newPos, Quaternion.identity);
+                }
+            }
+            
+        }
+    }
+
+}
